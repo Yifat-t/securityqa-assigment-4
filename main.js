891f2a8e24f73e5b4999b5945158f4576f21a54a
Yifat-t <78460287+Yifat-t@users.noreply.github.com>
2021-08-15 23:53:10 -0400

    Added data and table, added table css

--- main.js
+++ main.js
@@ -2,14 +2,27 @@ import './style.scss'
 
 import courses from "./courses.json";
 
-console.log(courses);
+var coursesHTML = `<table>
+                    <tr>
+                      <th>Course Name</th>
+                      <th>Course Number</th>
+                      <th>Lecturer</th>
+                    </tr>`;
+
+for (var i = 0; i < courses.courses.length; i++) {
+  coursesHTML += `<tr><td>` + courses.courses[i].name + `</td><td>` + courses.courses[i].number + `</td><td>` + courses.courses[i].lecturer + `</td></tr>`;
+}
+
+coursesHTML += `</table>`;
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
+  <h1>Hello Assignment #4!</h1>
 
   <a href="qa.html">Quality Assurance</a>
 
   <a href="security.html">Security</a>
 
   <a href="bad_example.html">Bad Example</a>
-`
+
+  <h2>Courses This Semester</h2>
+` + coursesHTML;
