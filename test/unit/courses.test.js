d8c9125877cfb001c706941e72101cb2922a846a
Yifat-t <78460287+Yifat-t@users.noreply.github.com>
2021-08-17 23:26:02 -0400

    Added eslint ignore patterns, fixed courses unit test, updated eslint package script

--- test/unit/courses.test.js
+++ test/unit/courses.test.js
@@ -1,9 +1,9 @@
-const expect = require('chai').expect;
+const expect = require('chai').expect
 
-const courses = require("../../courses.json");
- 
-it('should be a a non empty list', function() {
-    expect(courses).to.be.not.null;
-    expect(courses.courses).to.be.not.empty;
-    expect(courses.courses.length).to.be.eq(3);
-});
+const courses = require('../../courses.json')
+
+it('should be a a non empty list', function () {
+  expect(courses).to.be.not.null
+  expect(courses.courses).to.be.not.empty
+  expect(courses.courses.length).to.be.eq(3)
+})
